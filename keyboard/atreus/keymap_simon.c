e4add70be9d49f50bef651a456f706301642bca0
Simon Génier <simon@webscale.money>
2016-08-08 10:58:32 -0400

    Avoid super+shift.

--- keyboard/atreus/keymap_simon.c
+++ keyboard/atreus/keymap_simon.c
@@ -2,21 +2,17 @@
 
 #include "keymap_common.h"
 
-// This is the "classic" layout, which has the arrow keys on L2
-// instead of on the fn layer. However, it has no characters which
-// require both shift and fn to be inserted, unlike the newer default.
-
-//    !     @     {     }     |       ||       "     7     8     9    *
-//    #     $     (     )     `       ||       '     4     5     6    +
+//    !     @     {     }     "       ||       _     7     8     9    *
+//    #     $     (     )     '       ||       -     4     5     6    +
 //    %     ^     [     ]     ~       ||       &     1     2     3  backsl
-//   L2  insert super shift bksp ctrl || alt space   fn    .     0    =
+//    |     `   super shift bksp ctrl || alt space         .     0    =
 
 // L2 has the arrows:
 
 //  insert home    ↑    end  pgup     ||       ↑     F7    F8    F9   F10
 //    del   ←     ↓     →  pgdn     ||       ↓     F4    F5    F6   F11
 //                                    ||             F1    F2    F3   F12
-//              super shift bksp ctrl || alt space   L0             reset
+//  reset       super shift bksp ctrl || alt space   L0             reset
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KEYMAP(
@@ -26,20 +22,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC, KC_TAB, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT,              \
     KC_SPC, KC_FN0, KC_FN1, KC_NO, KC_ENT),                                   \
   KEYMAP(
-    SHIFT(KC_1), SHIFT(KC_2), SHIFT(KC_LBRC), SHIFT(KC_RBRC), SHIFT(KC_BSLS), \
-    SHIFT(KC_QUOT), KC_7, KC_8, KC_9, SHIFT(KC_8),                            \
-    SHIFT(KC_3), SHIFT(KC_4), SHIFT(KC_9), SHIFT(KC_0), KC_GRAVE,             \
-    KC_QUOT, KC_4, KC_5, KC_6, SHIFT(KC_EQUAL),                               \
+    SHIFT(KC_1), SHIFT(KC_2), SHIFT(KC_LBRC), SHIFT(KC_RBRC), SHIFT(KC_QUOT), \
+    SHIFT(KC_MINS), KC_7, KC_8, KC_9, SHIFT(KC_8),                            \
+    SHIFT(KC_3), SHIFT(KC_4), SHIFT(KC_9), SHIFT(KC_0), KC_QUOT,              \
+    KC_MINS, KC_4, KC_5, KC_6, SHIFT(KC_EQUAL),                               \
     SHIFT(KC_5), SHIFT(KC_6), KC_LBRC, KC_RBRC, SHIFT(KC_GRAVE),              \
     SHIFT(KC_7), KC_1, KC_2, KC_3, KC_BSLS,                                   \
-    KC_MINS, SHIFT(KC_INS), KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT,      \
+    SHIFT(KC_BSLS), KC_GRAVE, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT,    \
     KC_SPC, KC_FN0, KC_FN1, KC_0, KC_EQUAL),
   KEYMAP(
     KC_INS, KC_HOME, KC_UP, KC_END, KC_PGUP,                                  \
     KC_UP, KC_F7, KC_F8, KC_F9, KC_F10,                                       \
     KC_DEL, KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDN,                              \
     KC_DOWN, KC_F4, KC_F5, KC_F6, KC_F11,                                     \
-    KC_NO, KC__VOLUP, KC_NO, KC_NO, KC_NO,                                    \
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                            \
     KC_NO, KC_F1, KC_F2, KC_F3, KC_F12,                                       \
     KC_FN2, KC__VOLDOWN, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL,                  \
     KC_LALT, KC_SPC, KC_FN0, KC_FN1, KC_0, KC_PAUSE)
